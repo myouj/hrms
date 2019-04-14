49f6b9f52c1344e24dd9dc13c0b1c5b76f0dd0f0
myouj <18565544225@163.com>
2019-04-14 18:30:58 +0800

    搭建框架，首次上传

---
+++ src/main/java/com/lqt/hr/service/ITestService.java
@@ -0,0 +1,11 @@
+package com.lqt.hr.service;
+
+import com.lqt.hr.model.Test;
+
+import java.util.List;
+
+public interface ITestService {
+
+    List<Test> getList();
+
+}
---
+++ src/main/java/com/lqt/hr/service/impl/TestServiceImpl.java
@@ -0,0 +1,23 @@
+package com.lqt.hr.service.impl;
+
+import com.lqt.hr.dao.TestMapper;
+import com.lqt.hr.model.Test;
+import com.lqt.hr.model.TestExample;
+import com.lqt.hr.service.ITestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestServiceImpl implements ITestService {
+
+    @Autowired
+    private TestMapper testMapper;
+
+
+    public List<Test> getList() {
+        TestExample testExample = new TestExample();
+        return testMapper.selectByExample(testExample);
+    }
+}
