a9b111356723dbaaacb379a15deded28add08493
myouj <18565544225@163.com>
2019-05-27 12:51:18 +0800

    代码生成

---
+++ src/main/java/com/lqt/hr/model/Permission.java
@@ -0,0 +1,53 @@
+package com.lqt.hr.model;
+
+public class Permission {
+    private Integer id;
+
+    private String method;
+
+    private String url;
+
+    private Integer roleId;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method == null ? null : method.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
---
+++ src/main/java/com/lqt/hr/model/Role.java
@@ -0,0 +1,43 @@
+package com.lqt.hr.model;
+
+public class Role {
+    private Integer id;
+
+    private String name;
+
+    private Integer level;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
